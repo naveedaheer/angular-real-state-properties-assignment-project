f935aa23f461049ef8f2c096577e6cb48a14c1e6
Naveed Aheer <naveedaheer@gmail.com>
2020-04-16 00:34:58 +0500

    Implemented routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
+import { PropertyListComponent } from './properties/property-list/property-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: '**', component: PropertyListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
