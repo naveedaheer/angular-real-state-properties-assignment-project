913106e6be6bc3903c8e73ac698cc45fb51e7382
Naveed Aheer <naveedaheer@gmail.com>
2020-04-16 02:03:23 +0500

    Added a route for form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PropertyDetailComponent } from './property-detail/property-detail.component';
 import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { PropertyFormComponent } from './property-form/property-form.component';
 
 
 const routes: Routes = [
   { path: '', component: PropertyListComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'add-property', component: PropertyFormComponent },
   { path: '**', component: PropertyListComponent }
 ];
 
