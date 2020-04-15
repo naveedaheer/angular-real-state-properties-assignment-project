335373199d98e399dfec39503ca1eef9de2fcf89
Naveed Aheer <naveedaheer@gmail.com>
2020-04-16 00:14:14 +0500

    Integrated API for list view

--- client/src/app/properties/property-list/property-list.component.ts
+++ client/src/app/properties/property-list/property-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PropertyService } from './../../services/property.service';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-listView = false;
-  constructor() { }
+  listView = false;
+  properties = [];
+  constructor(private service: PropertyService) { }
 
   ngOnInit(): void {
+    this.service.getProperties().subscribe((res) => { this.properties = res.properties });
   }
 
 }
