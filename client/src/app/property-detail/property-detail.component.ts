0e897091e87190774cc34a62b6b29e8bc87db46d
Naveed Aheer <naveedaheer@gmail.com>
2020-04-16 02:04:04 +0500

    Integrated API for detail view

--- client/src/app/property-detail/property-detail.component.ts
+++ client/src/app/property-detail/property-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertyService } from './../services/property.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-detail.component.css']
 })
 export class PropertyDetailComponent implements OnInit {
-
-  constructor() { }
+  property;
+  constructor(private service: PropertyService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.service.getPropertyById(this.route.snapshot.paramMap.get('id')).subscribe(res => {
+      this.property = res.property[0];
+      console.log("this.property", this.property)
+    })
   }
 
 }
