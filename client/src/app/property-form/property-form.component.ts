26cb52e9fce26c930c56b37b7c360734f2a7da7f
Naveed Aheer <naveedaheer@gmail.com>
2020-04-16 19:59:31 +0500

    Done: update a property

--- client/src/app/property-form/property-form.component.ts
+++ client/src/app/property-form/property-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { PropertyService } from './../services/property.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-property-form',
@@ -9,7 +10,7 @@ import { PropertyService } from './../services/property.service';
   styleUrls: ['./property-form.component.css']
 })
 export class PropertyFormComponent implements OnInit {
-  propertForm = this.fb.group({
+  propertyForm = this.fb.group({
     type: ['', Validators.required],
     city: ['', Validators.required],
     price: ['', Validators.required],
@@ -17,13 +18,24 @@ export class PropertyFormComponent implements OnInit {
     refNumber: ['', Validators.required],
     description: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(100)]],
     image: ['', Validators.required]
-
   });
-  constructor(private fb: FormBuilder, private http: HttpClient, private service: PropertyService) { }
+  propertyId: string;
+  success = false;
+  constructor(private fb: FormBuilder, private http: HttpClient, private service: PropertyService, private route: ActivatedRoute, ) { }
 
 
 
   ngOnInit(): void {
+    this.propertyId = this.route.snapshot.paramMap.get('id');
+    if (this.propertyId) {
+      this.service.getPropertyById(this.propertyId).subscribe((res: any) => {
+        if (res.success) {
+          const obj = res.property[0];
+          delete obj._id
+          this.propertyForm.setValue(obj);
+        }
+      })
+    }
   }
 
   createBlob(file, callback) {
@@ -36,7 +48,7 @@ export class PropertyFormComponent implements OnInit {
     this.createBlob(event.srcElement.files[0], (blobString) => {
       this.http.post('https://api.cloudinary.com/v1_1/dauv0boag/image/upload', { file: blobString, upload_preset: 'gtxlmeb9', folder: 'properties' }).subscribe(res => {
         console.log('res', res['secure_url'])
-        this.propertForm.patchValue({
+        this.propertyForm.patchValue({
           image: res['secure_url']
         })
       })
@@ -44,11 +56,20 @@ export class PropertyFormComponent implements OnInit {
   }
 
   submit() {
-    this.service.addProperty(this.propertForm.value).subscribe((res: any) => {
-      if (res.success) {
-        this.propertForm.reset()
-      }
-    })
+    if (!this.propertyId) {
+      this.service.addProperty(this.propertyForm.value).subscribe((res: any) => {
+        if (res.success) {
+          this.propertyForm.reset();
+          this.success = true;
+        }
+      })
+    } else {
+      this.service.updateProperty(this.propertyForm.value, this.propertyId).subscribe((res: any) => {
+        if (res.success) {
+          this.success = true;
+        }
+      })
+    }
   }
 
 }
